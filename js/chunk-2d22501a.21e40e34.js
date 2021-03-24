54f9c5943a14fcd7536cec524247658475d1cb85
vedmaque <vedmaque@users.noreply.github.com>
2021-03-24 14:25:04 +0000

    deploy: ab19c643bfd37d18d05e63fc2605e19c0c98c4a5

---
+++ js/chunk-2d22501a.21e40e34.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22501a"],{e30f:function(e,n,t){"use strict";t.r(n);var r=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},u=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("h1",[e._v("This is Help and Support page")])])}],a=t("2877"),c={},l=Object(a["a"])(c,r,u,!1,null,null,null);n["default"]=l.exports}}]);
+//# sourceMappingURL=chunk-2d22501a.21e40e34.js.map
---
+++ js/chunk-2d237cc7.271c2580.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d237cc7"],{fd3f:function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},l=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("h1",[e._v("This is Blog page")])])}],r=t("2877"),s={},u=Object(r["a"])(s,c,l,!1,null,null,null);n["default"]=u.exports}}]);
+//# sourceMappingURL=chunk-2d237cc7.271c2580.js.map
